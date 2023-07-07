53b380a04f418ddd7efe3400c8d6986a74c75cc3
Gerson G <garciagers@gmail.com>
2023-07-06 22:42:30 -0500

    Delete Button Implemented

--- scripts/index.js
+++ scripts/index.js
@@ -66,9 +66,14 @@ function getCardElement(cardData) {
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
   const likeButton = cardElement.querySelector(".card__like-button");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
   likeButton.addEventListener("click", () => {
     likeButton.classList.toggle("card__like-button_active");
+  });
+
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
   });
 
   cardImageEl.alt = cardData.name;
